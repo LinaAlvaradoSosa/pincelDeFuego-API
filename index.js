636f542d7fc68f1378f1d052767ba79da8a98bc5
linaA <hellolinita@gmail.com>
2025-05-19 17:09:27 -0500

    cors

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import express from "express";
 import { connectDB } from "./config/db.js";
-import admin from "./routes/admin.routes.js"
-import productos from "./routes/productos.routes.js"
+import admin from "./routes/admin.routes.js";
+import productos from "./routes/productos.routes.js";
+import cors from "cors"
 
 const app = express();
+app.use(cors())
 connectDB();
 
 app.use(express.json())
